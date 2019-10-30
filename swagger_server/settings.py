951bb1e04f77c42a1dc3bb6e2a15dd2bee58fba9
Iakhin, Ruslan <ruslan.iakhin@sap.com>
2019-10-30 09:11:28 +0100

    buildspec.yml

--- swagger_server/settings.py
+++ swagger_server/settings.py
@@ -1,5 +1,5 @@
 import os
 
-DB_HOST = os.getenv('DB_HOST', 'localhost')
+DB_HOST = os.getenv('DB_HOST', 'hellowroldbirth-redis.w6vutn.0001.use2.cache.amazonaws.com')
 DB_PORT = os.getenv('DB_PORT', 6379)
 DB_PASSWORD = os.getenv('DB_PASSWORD')
